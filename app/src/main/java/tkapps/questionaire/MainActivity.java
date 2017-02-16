822c4f3e21ea32b4ed43d82e311c249d89cbcf7d
Karsten <amreink@dhbw-loerrach.de>
2017-02-16 17:47:41 +0100

    MainActivity bearbeitet, Main2Activity erstellt, Settings erstellt. activity_main.xml, activity_main2.xml, activitiy_settings.xml erstellt

--- app/src/main/java/tkapps/questionaire/MainActivity.java
+++ app/src/main/java/tkapps/questionaire/MainActivity.java
@@ -1,33 +1,49 @@
 package tkapps.questionaire;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.widget.ArrayAdapter;
-import android.widget.ListView;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-
         setContentView(R.layout.activity_main);
-        String [] answerlistArray = {
-                "Antwort 1",
-                "Antwort 2",
-                "Antwort 3",
-                "Antwort 4",
-                "Antwort 5"
-        };
 
-        ArrayAdapter <String> answerlistAdapter = new ArrayAdapter<String>(
-                this,
-                R.layout.answer_items,
-                answerlistArray);
+        //Button initialisieren
+        Button button_start = (Button) findViewById(R.id.button_start);
+        Button button_settings = (Button) findViewById(R.id.button_settings);
+        Button button_leaderboard = (Button) findViewById(R.id.button_leaderboard);
 
-        ListView answer_list = (ListView)findViewById(R.id.answer_list);
-        answer_list.setAdapter(answerlistAdapter);
+        //Button button_start ruft die Main2Activity auf
+        button_start.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, Main2Activity.class);
+                startActivity(intent);
+            }
+        });
+
+        //Button button_settings ruft die Einstellungen auf
+        button_settings.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, Settings.class);
+                startActivity(intent);
+            }
+        });
+
+        //Button button_settings ruft die Bestenliste auf
+        button_leaderboard.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, Settings.class);
+                startActivity(intent);
+            }
+        });
 
     }
 }
